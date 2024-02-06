75a93f7fd92d8b5f021b03486e7779ba8eb0499d
Kathryn Atherton <40177040+k-atherton@users.noreply.github.com>
2024-02-06 11:15:14 -0500

    Update 3_annotation_analysis.sh

--- 3_annotation_analysis.sh
+++ 3_annotation_analysis.sh
@@ -25,7 +25,6 @@ echo "=========================================================="
 # Move to directory with R analysis script
 cwd=$(pwd)
 scriptDir=${cwd}/annotation_analysis
-cd $scriptDir
 
 # Set path to your file where your ASV table with taxonomy is stored (must be a csv)
 data=$1
@@ -33,7 +32,7 @@ data=$1
 # Run R script
 echo "Running Normalize Gene Counts Script"
 module load R
-Rscript normalize_gene_counts.R
+Rscript ${scriptDir}/normalize_gene_counts.R
 echo "Running Get Gene Counts for Data Script"
-Rscript get_gene_counts_for_data.R ${data} 
+Rscript ${scriptDir}/get_gene_counts_for_data.R ${data} 
 echo "Done."
