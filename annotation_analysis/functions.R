842588f5fa28f1ab2bcda69846fd83a939d104c4
k-atherton <ktatherton@gmail.com>
2024-03-12 12:52:33 -0400

    adjust normalize_gene_counts for any annotation type

--- annotation_analysis/functions.R
+++ annotation_analysis/functions.R
@@ -8,52 +8,141 @@ call_in_files <- function(file){
   return(print(file))
 }
 
-get_GOacc <- function(file){
-  gene.table <- read.delim(file,sep="\t",header=TRUE)
-  gene.table$numHits <- 1
-  data <- aggregate(numHits~goAcc, gene.table, sum)
-  data <- data[order(data$goAcc),]
-  rownames(data) <- data[,1]
-  data <- data.frame(goAcc=data[,1])
-  return(data)
-}
-
-get_GOgid <- function(file){
-  gene.table <- read.delim(file,sep="\t",header=TRUE)
-  gene.table$numHits <- 1
-  data <- aggregate(numHits~GOgid, gene.table, sum)
-  data <- data[order(data$GOgid),]
-  rownames(data) <- data[,1]
-  data <- data.frame(GOgid=data[,1])
-  return(data)
-}
-
-get_pfam_names <- function(file){
-  gene.table <- read.delim(file,sep="\t",header=TRUE)
-  gene.table <- data.frame(subset(gene.table, gene.table$domainDb %in% c("HMMPfam")))
-  data <- aggregate(gene.table$numHits, by = list(gene.table$domainId), FUN = "sum")
-  rownames(data) <- data[,1]
-  data <- data.frame(domainId=data[,1])
-  return(data)
-}
-
-get_gene_count <- function(file,gene_names){
-  gene.table <- read.delim(paste0(path,"/",file),sep="\t",header=TRUE)
-  gene.table$numHits <- 1
-  data <- aggregate(as.numeric(gene.table$numHits), by = list(gene.table$goAcc), FUN = "sum")
-  rownames(data) <- data[,1]
-  spp_name_file <- strsplit(x=file,
-                       split = "_")[[1]]
-  index <- which((spp_name_file == "GeneCatalog") | (spp_name_file == "filtered") | (spp_name_file == "GO.tab.gz") | (spp_name_file == "FilteredModels1"))
-  if(index[1] > 2){
-    spp_name <- paste(spp_name_file[1:(index[1] - 1)], collapse = "_")
+get_genes <- function(file, a_type){
+  if(a_type == "GO"){
+    gene.table <- read.delim(file,sep="\t",header=TRUE)
+    gene.table$numHits <- 1
+    data <- aggregate(numHits~goAcc, gene.table, sum)
+    data <- data[order(data$goAcc),]
+    rownames(data) <- data[,1]
+    data <- data.frame(goAcc=data[,1])
+    return(data)
+  } else if(a_type == "KEGG"){
+    gene.table <- read.delim(file,sep="\t",header=TRUE)
+    gene.table$numHits <- 1
+    data <- aggregate(numHits~ecNum, gene.table, sum)
+    data <- data[order(data$ecNum),]
+    rownames(data) <- data[,1]
+    data <- data.frame(ecNum=data[,1])
+    return(data)
+  } else if(a_type == "InterPro"){
+    gene.table <- read.delim(file,sep="\t",header=TRUE)
+    gene.table$numHits <- 1
+    data <- aggregate(numHits~iprId, gene.table, sum)
+    data <- data[order(data$iprId),]
+    rownames(data) <- data[,1]
+    data <- data.frame(iprId=data[,1])
+    return(data)
+  } else if(a_type == "KOG"){
+    gene.table <- read.delim(file,sep="\t",header=TRUE)
+    gene.table$numHits <- 1
+    data <- aggregate(numHits~kogid, gene.table, sum)
+    data <- data[order(data$kogid),]
+    rownames(data) <- data[,1]
+    data <- data.frame(kogid=data[,1])
+    return(data)
+  } else if(a_type == "SignalP"){
+    gene.table <- read.delim(file,sep="\t",header=TRUE, skip = 1)
+    gene.table$numHits <- 1
+    data <- aggregate(numHits~proteinid, gene.table, sum)
+    data <- data[order(data$proteinid),]
+    rownames(data) <- data[,1]
+    data <- data.frame(proteinid=data[,1])
+    return(data)
   } else{
-    spp_name <- spp_name_file[1]
+    print("Error: unrecognized annotation type. See documentation for accepted annotationt types.")
   }
-  colnames(data)[1] <- "goAcc"
-  colnames(data)[2] <-paste0(spp_name)
-  data <- dplyr::left_join(gene_names,data,by = "goAcc")
-  return(data)
+}
+
+get_gene_count <- function(file,gene_names,a_type){
+  if(a_type == "GO"){
+    gene.table <- read.delim(paste0(path,"/",file),sep="\t",header=TRUE)
+    gene.table$numHits <- 1
+    data <- aggregate(as.numeric(gene.table$numHits), by = list(gene.table$goAcc), FUN = "sum")
+    rownames(data) <- data[,1]
+    spp_name_file <- strsplit(x=file,
+                              split = "_")[[1]]
+    index <- which((spp_name_file == "GeneCatalog") | (spp_name_file == "filtered") | (spp_name_file == paste0(a_type,".tab.gz")) | (spp_name_file == "FilteredModels1"))
+    if(index[1] > 2){
+      spp_name <- paste(spp_name_file[1:(index[1] - 1)], collapse = "_")
+    } else{
+      spp_name <- spp_name_file[1]
+    }
+    colnames(data)[1] <- "gene_id"
+    colnames(data)[2] <-paste0(spp_name)
+    data <- dplyr::left_join(gene_names,data,by = "gene_id")
+    return(data) 
+  } else if(a_type == "KEGG"){
+    gene.table <- read.delim(paste0(path,"/",file),sep="\t",header=TRUE)
+    gene.table$numHits <- 1
+    data <- aggregate(as.numeric(gene.table$numHits), by = list(gene.table$ecNum), FUN = "sum")
+    rownames(data) <- data[,1]
+    spp_name_file <- strsplit(x=file,
+                              split = "_")[[1]]
+    index <- which((spp_name_file == "GeneCatalog") | (spp_name_file == "filtered") | (spp_name_file == paste0(a_type,".tab.gz")) | (spp_name_file == "FilteredModels1"))
+    if(index[1] > 2){
+      spp_name <- paste(spp_name_file[1:(index[1] - 1)], collapse = "_")
+    } else{
+      spp_name <- spp_name_file[1]
+    }
+    colnames(data)[1] <- "gene_id"
+    colnames(data)[2] <-paste0(spp_name)
+    data <- dplyr::left_join(gene_names,data,by = "gene_id")
+    return(data)
+  } else if(a_type == "IPR"){
+    gene.table <- read.delim(paste0(path,"/",file),sep="\t",header=TRUE)
+    gene.table$numHits <- 1
+    data <- aggregate(as.numeric(gene.table$numHits), by = list(gene.table$iprId), FUN = "sum")
+    rownames(data) <- data[,1]
+    spp_name_file <- strsplit(x=file,
+                              split = "_")[[1]]
+    index <- which((spp_name_file == "GeneCatalog") | (spp_name_file == "filtered") | (spp_name_file == paste0(a_type,".tab.gz")) | (spp_name_file == "FilteredModels1"))
+    if(index[1] > 2){
+      spp_name <- paste(spp_name_file[1:(index[1] - 1)], collapse = "_")
+    } else{
+      spp_name <- spp_name_file[1]
+    }
+    colnames(data)[1] <- "gene_id"
+    colnames(data)[2] <-paste0(spp_name)
+    data <- dplyr::left_join(gene_names,data,by = "gene_id")
+    return(data)
+  } else if(a_type == "KOG"){
+    gene.table <- read.delim(paste0(path,"/",file),sep="\t",header=TRUE)
+    gene.table$numHits <- 1
+    data <- aggregate(as.numeric(gene.table$numHits), by = list(gene.table$kogid), FUN = "sum")
+    rownames(data) <- data[,1]
+    spp_name_file <- strsplit(x=file,
+                              split = "_")[[1]]
+    index <- which((spp_name_file == "GeneCatalog") | (spp_name_file == "filtered") | (spp_name_file == paste0(a_type,".tab.gz")) | (spp_name_file == "FilteredModels1"))
+    if(index[1] > 2){
+      spp_name <- paste(spp_name_file[1:(index[1] - 1)], collapse = "_")
+    } else{
+      spp_name <- spp_name_file[1]
+    }
+    colnames(data)[1] <- "gene_id"
+    colnames(data)[2] <-paste0(spp_name)
+    data <- dplyr::left_join(gene_names,data,by = "gene_id")
+    return(data)
+  } else if(a_type == "Sigp"){
+    gene.table <- read.delim(paste0(path,"/",file),sep="\t",header=TRUE, skip=1)
+    gene.table$numHits <- 1
+    data <- aggregate(as.numeric(gene.table$numHits), by = list(gene.table$proteinid), FUN = "sum")
+    rownames(data) <- data[,1]
+    spp_name_file <- strsplit(x=file,
+                              split = "_")[[1]]
+    index <- which((spp_name_file == "GeneCatalog") | (spp_name_file == "filtered") | (spp_name_file == paste0(a_type,".tab.gz")) | (spp_name_file == "FilteredModels1"))
+    if(index[1] > 2){
+      spp_name <- paste(spp_name_file[1:(index[1] - 1)], collapse = "_")
+    } else{
+      spp_name <- spp_name_file[1]
+    }
+    colnames(data)[1] <- "gene_id"
+    colnames(data)[2] <-paste0(spp_name)
+    data <- dplyr::left_join(gene_names,data,by = "gene_id")
+    return(data)
+  } else{
+    print("Error: unrecognized annotation type. See documentation for accepted annotationt types.")
+  }
 }
 
 combine_second_column<-function(x,y){
@@ -67,13 +156,13 @@ make_merged_dataframe <- function(path){
   filenames <- list.files(path, full.names=TRUE) 
   file_list <- list.files(path=path, pattern="*.tab")
   lapply(filenames, read_file_dat)
-  gene_list <- lapply(filenames, get_GOacc)
+  gene_list <- lapply(filenames, get_gene_id)
   gene_names <- plyr::ldply(gene_list, data.frame)
   gene_count_list <- lapply(file_list, gene_count, gene_names = gene_names)
   gene_count_dat <- Reduce(combine_second_column, gene_count_list)
   gene_count_dat[is.na(gene_count_dat)] <- 0
   gene_count_dat <- dplyr::distinct(gene_count_dat)
-  rownames(gene_count_dat) <- gene_count_dat$goAcc
+  rownames(gene_count_dat) <- gene_count_dat$gene_id
   gene_count_dat <- gene_count_dat[,-1]
   
   return(gene_count_dat)
--- annotation_analysis/normalize_gene_counts.R
+++ annotation_analysis/normalize_gene_counts.R
@@ -1,5 +1,22 @@
 source("functions.R")
 
+args <- commandArgs(trailingOnly = TRUE)
+annotation <- args[1] #annotation type (options: GO, KEGG, InterPro, KOG, Signalp)
+
+if(annotation == "GO"){
+  a_file <- "GO"
+} else if(annotation == "KEGG"){
+  a_file <- "KEGG"
+} else if(annotation == "InterPro"){
+  a_file <- "IPR"
+} else if(annotation == "KOG"){
+  a_file <- "KOG"
+} else if(annotation == "Signalp"){
+  a_file <- "Sigp"
+} else {
+  print("Error: unrecognized annotation type. Options: GO, KEGG, InterPro, KOG, Signalp")
+}
+
 ### SET PATH TO WHERE ALL ANNOTATIONN FILES FOR A SINGLE TYPE OF ANNOTATION ARE LOCATED
 path <- "mycocosm_annotations"
 
@@ -27,7 +44,7 @@ portal_names <- c()
 for(i in 1:length(file_list)){
   spp_name_file <- strsplit(x=file_list[i],
                             split = "_")[[1]]
-  index <- which((spp_name_file == "GeneCatalog") | (spp_name_file == "filtered") | (spp_name_file == "GO.tab.gz") | (spp_name_file == "FilteredModels1"))
+  index <- which((spp_name_file == "GeneCatalog") | (spp_name_file == "filtered") | (spp_name_file == paste0(a_file,".tab.gz")) | (spp_name_file == "FilteredModels1"))
   if(index[1] > 2){
     spp_name <- paste(spp_name_file[1:(index[1] - 1)], collapse = "_")
   } else{
@@ -35,22 +52,37 @@ for(i in 1:length(file_list)){
   }
   portal_names <- c(portal_names, spp_name)
 }
+portal_names_no_nums <- portal_names
+for(i in 1:length(portal_names_no_nums)){
+  portal <- strsplit(x=portal_names_no_nums[i], split = "-")
+  if(length(portal[[1]]) > 1){
+    portal_names_no_nums[i] <- portal[[1]][!grepl("^[[:digit:]]+", portal[[1]])]
+  }
+}
 
-n_occur <- data.frame(table(portal_names))
+n_occur <- data.frame(table(portal_names_no_nums))
 duplicates <- n_occur[n_occur$Freq > 1,]
+
+portals_duplicate <- c()
+
+for(i in 1:nrow(duplicates)){
+  portal <- as.character(duplicates$portal_names_no_nums[i])
+  index <- which(portal == portal_names_no_nums)
+  portals_duplicate <- c(portals_duplicate, portal_names[index])
+}
 
 if(nrow(duplicates) > 0){
   filenames_duplicates <- c()
   for(i in 1:length(file_list)){
     spp_name_file <- strsplit(x=file_list[i],
                               split = "_")[[1]]
-    index <- which((spp_name_file == "GeneCatalog") | (spp_name_file == "filtered") | (spp_name_file == "GO.tab.gz") | (spp_name_file == "FilteredModels1"))
+    index <- which((spp_name_file == "GeneCatalog") | (spp_name_file == "filtered") | (spp_name_file == paste0(a_file,".tab.gz")) | (spp_name_file == "FilteredModels1"))
     if(index[1] > 2){
       spp_name <- paste(spp_name_file[1:(index[1] - 1)], collapse = "_")
     } else{
       spp_name <- spp_name_file[1]
     }
-    if(spp_name %in% duplicates$portal_names){
+    if(spp_name %in% portals_duplicate){
       filenames_duplicates <- c(filenames_duplicates, file_list[i])
     }
   }
@@ -61,13 +93,23 @@ if(nrow(duplicates) > 0){
   for(i in 1:length(filenames_duplicates$filenames_duplicates)){
     spp_name_file <- strsplit(x=filenames_duplicates$filenames_duplicates[i],
                               split = "_")[[1]]
-    index <- which((spp_name_file == "GeneCatalog") | (spp_name_file == "filtered") | (spp_name_file == "GO.tab.gz") | (spp_name_file == "FilteredModels1"))
+    index <- which((spp_name_file == "GeneCatalog") | (spp_name_file == "filtered") | (spp_name_file == paste0(a_file,".tab.gz")) | (spp_name_file == "FilteredModels1"))
     if(index[1] > 2){
       spp_name <- paste(spp_name_file[1:(index[1] - 1)], collapse = "_")
     } else{
       spp_name <- spp_name_file[1]
     }
     filenames_duplicates$portal[i] <-spp_name
+  }
+  
+  filenames_duplicates$other <- NA
+  
+  for(i in 1:length(filenames_duplicates$portal)){
+    portal <- strsplit(x=filenames_duplicates$portal[i], split = "-")
+    if(length(portal[[1]]) > 1){
+      filenames_duplicates$portal[i] <- portal[[1]][!grepl("^[[:digit:]]+", portal[[1]])]
+      filenames_duplicates$other[i] <- portal[[1]][grepl("^[[:digit:]]+", portal[[1]])]
+    }
   }
   
   filenames_duplicates$date <- NA
@@ -84,9 +126,13 @@ if(nrow(duplicates) > 0){
   for(i in 1:length(unique_duplicates)){
     portal <- unique_duplicates[i]
     date_keep <- max(filenames_duplicates$date[which(filenames_duplicates$portal == portal)])
-    file_remove <- c(file_remove, 
-                     filenames_duplicates$filenames_duplicates[which((filenames_duplicates$date != date_keep) & (filenames_duplicates$portal == portal))])
-  }
+    delete <- filenames_duplicates$filenames_duplicates[which((filenames_duplicates$date != date_keep) & (filenames_duplicates$portal == portal))]
+    if(length(delete) < 1){
+      other_keep <- max(filenames_duplicates$other[which(filenames_duplicates$portal == portal)])
+      delete <- filenames_duplicates$filenames_duplicates[which((filenames_duplicates$other != other_keep) & (filenames_duplicates$portal == portal))]
+    }
+    file_remove <- c(file_remove, delete)
+  }                 
   if(length(file_remove) > 0){
     files_remove <- which(file_list %in% file_remove)
     filenames <- filenames[-files_remove]
@@ -95,25 +141,32 @@ if(nrow(duplicates) > 0){
 }
 
 ### MAKE A LIST OF GENE NAMES #################################################
-gene_list <- lapply(filenames, get_GOacc)
+gene_list <- lapply(filenames, get_genes, a_type=annotation)
 gene_names <- plyr::ldply(gene_list, data.frame)
 gene_names <- distinct(gene_names)
+colnames(gene_names) <- "gene_id"
 
 ### GET GENE COUNTS ###########################################################
-gene_count_list <- lapply(file_list, get_gene_count, gene_names = gene_names)
+gene_count_list <- lapply(file_list, get_gene_count, gene_names = gene_names, a_type = a_file)
 
 ### COLLATE DATA ##############################################################
 gene_count_data <- Reduce(combine_second_column, gene_count_list)
+for(i in 1:ncol(gene_count_data)){
+  portal <- strsplit(x=colnames(gene_count_data)[i], split = "-")
+  if(length(portal[[1]]) > 1){
+    colnames(gene_count_data)[i] <- portal[[1]][!grepl("^[[:digit:]]+", portal[[1]])]
+  }
+}
 gene_count_data[is.na(gene_count_data)] <- 0
 
 ### SAVE COUNTS CSV ###########################################################
-write.csv(gene_count_data, file = "data/GO_annotations_count_table.csv", row.names = FALSE)
+write.csv(gene_count_data, file = "data/annotations_count_table.csv", row.names = FALSE)
 
 ### NORMALIZE COUNTS TO GENOME SIZE (PER 10,000 GENES) ########################
-rownames(gene_count_data) <- gene_count_data$goAcc
+rownames(gene_count_data) <- gene_count_data$gene_id
 gene_count_data <- gene_count_data[,-1]
 gene_count_norm <- normalize_genome_count(gene_count_data)
-write.csv(gene_count_norm, file = "data/GO_annotations_count_table_norm.csv")
+write.csv(gene_count_norm, file = "data/annotations_count_table_norm.csv")
 
 ### IMPORT MYCOCOSM DATABASE ##################################################
 mycocosm <- read.csv("data/mycocosm_its_merge.csv")
@@ -133,9 +186,9 @@ for(i in 1:nrow(gene_count_t)){
 ### AVERAGE GENE COUNTS FOR SPECIES ##########################################
 species <- unique(gene_count_t$Species)
 average_species_count <- average_taxa(gene_count_t, species, "Species")
-write.csv(average_species_count, file = "data/GO_average_bySpecies.csv")
+write.csv(average_species_count, file = "data/annotation_average_bySpecies.csv")
 
 ### AVERAGE GENE COUNTS FOR GENERA ###########################################
 genera <- unique(gene_count_t$Genus)
 average_genus_count <- average_taxa(gene_count_t, genera, "Genus")
-write.csv(average_genus_count, file = "data/GO_average_byGenus.csv")
+write.csv(average_genus_count, file = "data/annotation_average_byGenus.csv")
