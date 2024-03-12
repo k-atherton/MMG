ddc6a032b08a270f6e74bf763b8a06aef5784fd6
k-atherton <ktatherton@gmail.com>
2024-03-12 11:18:44 -0400

    Adapt move files script for any annotation type

--- 2_move_annotation_files.sh
+++ 2_move_annotation_files.sh
@@ -5,6 +5,18 @@ if [ ! -d "$mycocosm" ]; then
   mkdir mycocosm_annotations
 fi
 
-mv $1/*/Annotation/Mycocosm/Annotation/Filtered_Models___best__/Functional_Annotations/GO/* ${mycocosm}
+if [ $2 = "GO" ]; then
+	mv $1/*/Annotation/Mycocosm/Annotation/Filtered_Models___best__/Functional_Annotations/GO/* ${mycocosm}
+elif [ $2 = "KEGG" ]; then
+	mv $1/*/Annotation/Mycocosm/Annotation/Filtered_Models___best__/Functional_Annotations/EC_annotations_and_KEGG_pathway_mappings/* ${mycocosm}
+elif [ $2 = "InterPro" ]; then
+	mv $1/*/Annotation/Mycocosm/Annotation/Filtered_Models___best__/Functional_Annotations/InterPro/* ${mycocosm}
+elif [ $2 = "KOG" ]; then
+	mv $1/*/Annotation/Mycocosm/Annotation/Filtered_Models___best__/Functional_Annotations/KOG/* ${mycocosm}
+elif [ $2 = "Signalp" ]; then
+	mv $1/*/Annotation/Mycocosm/Annotation/Filtered_Models___best__/Functional_Annotations/Signalp/* ${mycocosm}
+else 
+	echo "Annotation type not recognized: options are GO, KEGG, InterPro, KOG, and Signalp."
+fi
 
 rm -rf $1
