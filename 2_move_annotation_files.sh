df1999ebaac52c593a69d4158f931f88e5311c7f
Kathryn Atherton <40177040+k-atherton@users.noreply.github.com>
2024-03-12 11:26:07 -0400

    Update 2_move_annotation_files.sh

--- 2_move_annotation_files.sh
+++ 2_move_annotation_files.sh
@@ -7,16 +7,19 @@ fi
 
 if [ $2 = "GO" ]; then
 	mv $1/*/Annotation/Mycocosm/Annotation/Filtered_Models___best__/Functional_Annotations/GO/* ${mycocosm}
+ 	rm -rf $1
 elif [ $2 = "KEGG" ]; then
 	mv $1/*/Annotation/Mycocosm/Annotation/Filtered_Models___best__/Functional_Annotations/EC_annotations_and_KEGG_pathway_mappings/* ${mycocosm}
+ 	rm -rf $1
 elif [ $2 = "InterPro" ]; then
 	mv $1/*/Annotation/Mycocosm/Annotation/Filtered_Models___best__/Functional_Annotations/InterPro/* ${mycocosm}
+ 	rm -rf $1
 elif [ $2 = "KOG" ]; then
 	mv $1/*/Annotation/Mycocosm/Annotation/Filtered_Models___best__/Functional_Annotations/KOG/* ${mycocosm}
+ 	rm -rf $1
 elif [ $2 = "Signalp" ]; then
 	mv $1/*/Annotation/Mycocosm/Annotation/Filtered_Models___best__/Functional_Annotations/Signalp/* ${mycocosm}
+ 	rm -rf $1
 else 
 	echo "Annotation type not recognized: options are GO, KEGG, InterPro, KOG, and Signalp."
 fi
-
-rm -rf $1
