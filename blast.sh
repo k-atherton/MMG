89aa437790c188f67cad281a8366113024626c36
Kathryn Atherton <40177040+k-atherton@users.noreply.github.com>
2024-10-02 16:19:43 -0400

    Update blast.sh

--- blast.sh
+++ blast.sh
@@ -27,6 +27,7 @@ perc=$2
 
 cwd=$(pwd)
 
+echo "BLASTING FASTA TO MYCOCOSM ITS DATABASE"
 # Set path to data directory
 dataDir=${cwd}/data
 cd ${dataDir}
@@ -38,5 +39,14 @@ blastn -query ${query} \
           -qcov_hsp_perc ${perc}
 
 grep "^[^#]" blast_nn_all.txt >> blast_nn.txt
-
 rm -rf blast_nn_all.txt
+echo "=========================================================="
+
+echo "GETTING BEST BLAST MATCH PORTAL ID FOR ITS"
+# Set path to script directory
+scriptDir=${cwd}/download_scripts
+
+module load R
+Rscript 4_blast_to_portal.R ${dataDir}/blast_nn.txt ${dataDir}
+echo "Done."
+
